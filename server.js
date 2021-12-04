defafc672392fc7bb06623a614f3ad23e8db0b73
arevalolance <lancearevalo2000@yahoo.com>
2021-12-04 16:01:41 +0800

    fix: can now be run

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParse = require("body-parser");
+const app = express();
+
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParse.json());
+app.use(
+    bodyParse.urlencoded({
+        extended: true,
+    })
+);
+
+app.get('/', (req,res) => {
+    res.json({
+        message: "lmao abcd hello"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`CURRENTLY LISTENING AT PORT:${port}`)
+})
+app.listen(80, () => {
+    console.log(`CURRENTLY LISTENING AT PORT:80`)
+})
