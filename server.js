f5e9f9d61b0ea5c70cb34c271e58bcd0d3ac29f8
arevalolance <lancearevalo2000@yahoo.com>
2021-12-05 20:10:17 +0800

    add test query

--- server.js
+++ server.js
@@ -1,40 +1,47 @@
 const express = require("express");
 const bodyParse = require("body-parser");
 const app = express();
-const mysql = require("mysql")
+const mysql = require("mysql");
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParse.json());
 app.use(
-    bodyParse.urlencoded({
-        extended: true,
-    })
+  bodyParse.urlencoded({
+    extended: true,
+  })
 );
 
-app.get('/', (req,res) => {
-    res.json({
-        message: "lmao abcd hello"
-    })
-})
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "abcd",
-    password: "!abcd123"
+app.get("/", (req, res) => {
+  res.json({
+    message: "lmao abcd hello",
+  });
 });
 
-con.connect((res,err) => {
-    if (err) {
-        throw err;
-    }
-    console.log("CONNECTED TO ABCD")
-})
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "abcd",
+  password: "!abcd123",
+});
+
+con.connect((res, err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("CONNECTED TO ABCD");
+});
+
+app.get("/test", (req, res) => {
+  con.query("SELECT * FROM user", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
 app.listen(port, () => {
-    console.log(`CURRENTLY LISTENING AT PORT:${port}`)
-})
+  console.log(`CURRENTLY LISTENING AT PORT:${port}`);
+});
 app.listen(80, () => {
-    console.log(`CURRENTLY LISTENING AT PORT:80`)
-})
+  console.log(`CURRENTLY LISTENING AT PORT:80`);
+});
