58f1cdc11d6f2dadf8a6bcfb85171a0f5a2de4d3
arevalolance <lancearevalo2000@yahoo.com>
2021-12-08 00:18:41 +0800

    add dashboard and login

--- routes/session.js
+++ routes/session.js
@@ -1,37 +1,45 @@
 const router = require("express").Router();
 const { json } = require("body-parser");
-const e = require("express");
 const db = require("../services/db.js");
+var path = require("path")
+const express = require("express");
+const app = express();
+app.use(express.static("../public"))
 
-router.post("/login", (req, res) => {
+router.post("/attempt", (req, res) => {
   const { username, password } = req.body;
-
-  var queriedUser;
-  var queriedPassword;
-
-  var queriedData;
-  db.query("SELECT * FROM user WHERE `username`=? AND `password`=?", [username, password]).then((results) => {
-	const test = results[0]; 
-	  console.log(test)
-	console.log(req.body)
-	  if (
-        req.body.username === test.username &&
-        req.body.password === test.password 
-      ) {
-        session = req.session;
-        session.userid = username;
-        res.send(
-          `Hey there, welcome ${session.userid} <a href=\'/session/logout'>click to logout</a>`
-        );
-      } else {
-        res.send("Invalid username or password");
-      }
-  })
+  console.log(req.body)
+  db.query("SELECT * FROM user WHERE `username`=? AND `password`=?", [
+    username,
+    password,
+  ]).then((results) => {
+    const test = results[0];
+    if (
+      req.body.username === test.username &&
+      req.body.password === test.password
+    ) {
+      session = req.session;
+      session.userid = username;
+      res.send(
+        `Hey there, welcome ${session.userid} <a href=\'/session/logout'>click to logout</a>`
+      );
+    } else {
+      res.send("Invalid username or password");
+    }
+  });
 });
 
 router.get("/logout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.userid) {
+    res.redirect("/");
+  } else {
+    res.sendFile(path.resolve("public/login.html"))
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const app = express();
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
 const bodyParse = require("body-parser");
-const app = express();
 const mysql = require("mysql2");
+var path = require("path")
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const oneDay = 1000 * 60 * 60 * 24;
 
+app.use(express.static('public'))
+app.use(express.static(__dirname));
 app.use(sessions({
   secret: "abcd123thisisasecretkey",
   saveUninitialized: true,
@@ -23,13 +26,11 @@ app.use("/member", require("./routes/member.js"));
 app.use("/session", require("./routes/session.js"));
 
 app.get('/',(req,res) => {
-  session=req.session;
-  if(session.userid){
-      res.send(`Welcome User ${session.userid} <a href=\'/logout'>click to logout</a>`);
-  }else
-  res.sendFile('views/index.html',{root:__dirname})
+  res.sendFile(path.resolve("public/index.html"))
 });
 
 app.listen(port, () => {
   console.log(`CURRENTLY LISTENING AT PORT:${port}`);
 });
+
+module.exports = app;
