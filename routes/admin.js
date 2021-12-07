60ad4840920c992d7a4cb5e2780b7694b0959b4a
arevalolance <lancearevalo2000@yahoo.com>
2021-12-07 16:58:02 +0800

    fix routers

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,10 @@
 const router = require("express").Router;
-const bodyParse = require("body-parser");
-const app = express();
-const mysql = require("mysql2");
 const db = require("../services/db.js")
 
 router.get("/:id", (req, res) => {
-    const { id } = res.params;
-    const results = db.query("SELECT * FROM `admin` WHERE `user_id`=?", [id] );
+    const { id } = req.params;
+    const results = db.query("SELECT * FROM `events` WHERE `user_id`=?", [id] );
     console.log(results);
-    
-    // res.json({
-
-    // })
 })
 
 module.exports = router;
