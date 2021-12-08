d80c4d0e5d124d39d8b0efec9a1fe5789e081189
arevalolance <lancearevalo2000@yahoo.com>
2021-12-08 15:07:09 +0800

    fix select specific query

--- routes/events.js
+++ routes/events.js
@@ -5,7 +5,14 @@ const db = require("../services/db.js")
 router.get("/:id", (req, res) => {
     const { id } = req.params;
     db.query("SELECT * FROM `event` WHERE `event_id`=?", [id]).then((results) => {
-        res.json(results);
+        res.json({
+            eventName: results.json,
+            address: results.address,
+            date: results.date,
+            time: results.time,
+            description: results.description,
+            status: results.status
+        });
     });
 })
 
